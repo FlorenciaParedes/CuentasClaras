51f9d00f837de225611f91012c630e2432663639
cata <rattocatalina@gmail.com>
2023-10-20 10:45:55 -0300

    agregue actualizar, eliminar y eliminar con id

--- src/main/java/DAO/GenericDAO.java
+++ src/main/java/DAO/GenericDAO.java
@@ -1,5 +1,14 @@
 package DAO;
 
 public interface GenericDAO<T> {
+
 	public T guardar(T base);
+	
+	T actualizar(T entidad);
+
+    void borrar(T entidad);
+	
+	void eliminar(long id);
+
+
 }
--- src/main/java/inicio/Main.java
+++ src/main/java/inicio/Main.java
@@ -9,12 +9,25 @@ public class Main {
 	//Usuario usuario= new Usuario("Juan");
 	//Grupo grupo = new Grupo("Grupo1", (byte) 1);	Esta linea daria es para luego relacionar el usuario con un grupo, pero todavia no esta el metodo y la vuelta con los dao.
 	Usuario usuario = new Usuario("juan123", "Juan", "Pérez", "juan@mail.com", "contrasena123");
-
+	Usuario usuario2 = new Usuario("pedro123", "Pedro", "Garcia", "pedro@mail.com", "pedro123");
 	//Persiste Usuario y Servicio en BD
 	UsuarioDAOimpl uDAO = new UsuarioDAOimpl();
 	System.out.println("LLEGAMOS AL MAIN ANTES DEL GUARDAR");
 	uDAO.guardar(usuario);
 	System.out.println("LLEGAMOS AL MAIN LUEGO DEL GUARDAR");
+	
+	//pisa al otro este actualizar, preguntar si asi es la idea!
+	//uDAO.actualizar(usuario2);
+	System.out.println("LLEGAMOS AL MAIN LUEGO DEL actualizar");
+	
+	//uDAO.borrar(usuario2);
+	System.out.println("LLEGAMOS AL MAIN LUEGO DE borrar entidad");
+	
+	//uDAO.guardar(usuario);
+	//	System.out.println("Guardamos a juan de vuelta");
+	
+	uDAO.eliminar(1);
+	System.out.println("LLEGAMOS AL MAIN LUEGO DEL eliminar con id");
 	
 	}
 }
