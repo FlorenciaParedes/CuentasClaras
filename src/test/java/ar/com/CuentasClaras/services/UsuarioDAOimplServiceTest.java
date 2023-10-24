221d4babb0ce21d5e0877a7e38f00b1b797055bd
Flo <flomic08@gmail.com>
2023-10-24 10:56:27 -0300

    cambie el import del test

--- src/test/java/ar/com/CuentasClaras/services/UsuarioDAOimplServiceTest.java
+++ src/test/java/ar/com/CuentasClaras/services/UsuarioDAOimplServiceTest.java
@@ -1,6 +1,7 @@
 package ar.com.CuentasClaras.services;
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import DAOimplements.UsuarioDAOimpl;
@@ -13,15 +14,12 @@ public class UsuarioDAOimplServiceTest {
 	public void testGuardarUsuarioDAOimpl() {
 		
 		
-		  // Crear una instancia de UsuarioDAOimpl
+		 // Crear una instancia de UsuarioDAOimpl
         UsuarioDAOimpl usuarioDAO = new UsuarioDAOimpl();
-
         // Crear un nuevo usuario para guardar
         Usuario usuarioAGuardar = new Usuario("usuario123", "Nombre", "Apellido", "email@example.com", "contrasena123");
-
         // Llamo al método para guardar el usuario
         Usuario usuarioGuardado = usuarioDAO.guardar(usuarioAGuardar);
-
         // Asegurarse de que el usuario guardado no sea nulo
         Assertions.assertNotNull(usuarioGuardado);
 
